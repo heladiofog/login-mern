df61d91285e1219679b6c0ace2c80be8ca0c4359
Heladio Atreaga Franco <harteagaf@atencion.com>
2019-04-15 12:23:24 -0500

    Finishing uploading documents.

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -1,31 +1,55 @@
 const express = require('express');
 const IncomingForm = require('formidable');
-
+const fs = require('fs');
 // router
 const router = express.Router();
 
 router.post('/', function upload(req, res) {
   // A new form
-  let form = new IncomingForm()
+  let form = new IncomingForm();
+  try {
+    form.on('fileBegin', (name, file) => {
+      // temp folder
+      // file.path = '../../uploads/projects/' + file.name;
+      // copy the file to a specific path
+      // file.path = __dirname + '/uploads/projects/' + file.name;
+      // form.uploadDir = "/uploads/projects/";
 
-  // register callbacks on that form
-  form.on('file', (field, file) => {
-    // For each file on the form, do something
-    // save it in the db
-    // file.path to access the file
-    console.log("File Received");
-    // copy the file to a specific path
-  });
+      // console.log({ message: "FileBegin - Write on: " + form.uploadDir });
+    });
 
-  // Second callback is called when the form is 
-  // completely parsed
-  form.on('end', () => {
-    res.json({ message: "Donde." });
-  })
+    // register callbacks on that form
+    form.on('file', (field, file) => {
+      // For each file on the form, do something
+      // save it in the db
+      // file.path to access the file
+      console.log("File Received: " + file.name);
+    });
 
-  // Then, tigger the parsing of the form:
-  form.parse(req)
+    // Second callback is called when the form is 
+    // completely parsed
+    form.on('end', () => {
+      // console.log({ message: "End - Done." });
+      // res.json({ message: "End - Done." });
+    })
 
+    // Then, tigger the parsing of the form:
+    form.parse(req, (err, fields, files) => {
+      console.log({ message: "Parsed - Done. File temp location: " + JSON.stringify(files.file.name) });
+      var oldpath = files.file.path;
+      var newpath = './uploads/projects/' + files.file.name;
+      // console.log(`OldPath: ${oldpath} - newPath: ${newpath}`);
+      // Move from temp path to desired location
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        // res.write('File uploaded and moved!');
+        res.json({ message: "File successfully uploaded!" });
+      });
+    });
+  } catch (error) {
+    console.log("ERROR on upload: " + error)
+    res.status(500).json({ message: "No se pudo subir el documento."})
+  }
 });
 
 module.exports = router;
